6389a951a65f88feb328d9a41f2b11f14d4fd8bf
Rajat32-op <rajathegde2006@gmail.com>
2026-02-14 21:37:32 +0530

    energy monitoring

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include"core/monitor/cpu_utilize.h"
+#include"core/monitor/energy.h"
 #include<stdlib.h>
 #include<unistd.h>
 
 int main(){
     printf("hello from main\n");
     CpuUtilizationMonitor cpu_util_monitor ;
+    EnergyMonitor energy_monitor;
     // while(1){
     //     double res=cpu_util_monitor.monitorFullCpuUtil();
     //     if(res==-1.0){
@@ -14,18 +16,28 @@ int main(){
     //     printf("%lf percentage of cpu is being utilised\n",res);
     //     sleep(1);
     // }
-    while (1) {
-    std::vector<double> res = cpu_util_monitor.monitorCoreCpuUtil();
-    if (res.empty() || res[0] == -1.0) {
-        return 1;
-    }
-    printf("--------------------------------------------------\n");
-    printf("Per-Core CPU Utilization:\n");
-    for (size_t i = 0; i < res.size(); ++i) {
-        printf("Core %-2zu : %6.2f %%\n", i, res[i]);
-    }
-    printf("--------------------------------------------------\n\n");
-    sleep(1); 
+
+    // while (1) {
+    // std::vector<double> res = cpu_util_monitor.monitorCoreCpuUtil();
+    // if (res.empty() || res[0] == -1.0) {
+    //     return 1;
+    // }
+    // printf("--------------------------------------------------\n");
+    // printf("Per-Core CPU Utilization:\n");
+    // for (size_t i = 0; i < res.size(); ++i) {
+    //     printf("Core %-2zu : %6.2f %%\n", i, res[i]);
+    // }
+    // printf("--------------------------------------------------\n\n");
+    // sleep(1); 
+    // }
+    printf("Energy counted from start of program\n");
+    while(1){
+        double res=energy_monitor.monitorEnergy();
+        if(res==-1.0){
+            return 1;
+        }
+        printf("%lf Joules of energy is used\n",res);
+        sleep(1);
     }
     return 0;
 }
