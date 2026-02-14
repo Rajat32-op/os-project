1e84313274c52c71e413e4881aefc9a361c59fd4
Rajat32-op <rajathegde2006@gmail.com>
2026-02-14 11:48:17 +0530

    per core monitoring done

--- main.cpp
+++ main.cpp
@@ -6,13 +6,26 @@
 int main(){
     printf("hello from main\n");
     CpuUtilizationMonitor cpu_util_monitor ;
-    while(1){
-        double res=cpu_util_monitor.monitor_full_cpu_util();
-        if(res==-1.0){
-            return 1;
-        }
-        printf("%lf percentage of cpu is being utilised\n",res);
-        sleep(1);
+    // while(1){
+    //     double res=cpu_util_monitor.monitor_full_cpu_util();
+    //     if(res==-1.0){
+    //         return 1;
+    //     }
+    //     printf("%lf percentage of cpu is being utilised\n",res);
+    //     sleep(1);
+    // }
+    while (1) {
+    std::vector<double> res = cpu_util_monitor.monitor_core_cpu_util();
+    if (res.empty() || res[0] == -1.0) {
+        return 1;
+    }
+    printf("--------------------------------------------------\n");
+    printf("Per-Core CPU Utilization:\n");
+    for (size_t i = 0; i < res.size(); ++i) {
+        printf("Core %-2zu : %6.2f %%\n", i, res[i]);
+    }
+    printf("--------------------------------------------------\n\n");
+    sleep(1); 
     }
     return 0;
 }
