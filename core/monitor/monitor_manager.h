b82ff192e59ab9dbf144627cb6b72a00548aba42
Rajat32-op <rajathegde2006@gmail.com>
2026-03-01 11:34:18 +0530

    used turbostat for monitoring

--- core/monitor/monitor_manager.h
+++ core/monitor/monitor_manager.h
@@ -1,40 +1,21 @@
-#include"energy.h"
-#include"cpu_utilize.h"
+#include<pthread.h>
 #include<thread>
 #include<atomic>
-#include<vector>
-#include<fstream>
-#include<chrono>
-
-struct snapShot{
-    CpuUtilizationMonitor cpu_util_monitor;
-    EnergyMonitor energy_monitor;
-    int timestamp; //in ms
-};
-
-struct systemMetric{
-    double cpu_util;
-    std::vector<double>per_core_util;
-    double energy;
-    // add frequency also
-};
+#include<string>
+#include<iostream>
 
 class MonitorManager{
 private:
-    snapShot prev;
-    snapShot cur;
-    std::thread monitor_thread;
-    std::atomic<bool>running;
-    int interval=1000;  //in mili seconds
-    std::chrono::steady_clock::time_point startTime;
-
-    std::ofstream livestream;
-    //need to declare results array and store whatever is needed for report
-
-    void monitorLoop();
-    void takeSnapShot(snapShot &snap);
-    systemMetric getMetric();
+    pid_t turbostat_pid;
+    std::atomic<bool> running;   
+    std::thread reader_thread;
+    int fifo_fd;
+    int pipefd[2];
+    
+    void readerLoop();
 public:
+    MonitorManager();
+    ~MonitorManager();
     void start();
     void stop();
 };
